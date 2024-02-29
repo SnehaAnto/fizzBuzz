3d3ec75869aed605be32feb9b8ea4ad96c80bcaa
snehamaddhichettyant <snea913@gmail.com>
2024-02-28 19:40:04 -0500

    Rename class, rewrite test

--- src/main/java/org/example/FizzBuzz.java
+++ src/main/java/org/example/FizzBuzz.java
@@ -4,16 +4,15 @@ package org.example;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args ) {
+public class FizzBuzz {
+    public static void main(String[] args) {
         for (int i = 1; i <= 100; i++) {
             System.out.println(fizzBuzz(i));
         }
     }
+
     public static String fizzBuzz(int num) {
-            return "hello";
+       return "Hello";
+
     }
-
-
 }
