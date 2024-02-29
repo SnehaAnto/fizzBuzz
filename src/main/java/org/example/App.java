effdc68e678a9fbf2acd781922d2b221f66d38f5
snehamaddhichettyant <snea913@gmail.com>
2024-02-28 19:03:30 -0500

    Initial code

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,19 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(fizzBuzz(i));
+        }
+    }
+    public static String fizzBuzz(int num) {
+            return "hello";
+    }
+
+
+}
